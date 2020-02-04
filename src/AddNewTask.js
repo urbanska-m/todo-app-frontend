6947bd79c2da083048a62609be7909c95110aacc
Monika Urbanska <monika.urbanska1@gmail.com>
2020-02-04 20:55:12 +0000

    changing functions to use event handlers

--- src/AddNewTask.js
+++ src/AddNewTask.js
@@ -2,30 +2,32 @@ import React from 'react';
 
 class AddNewTask extends React.Component {
 
-  state = {
-    taskDescription: ""
-  };
+  // state = {
+  //   taskDescription: ""
+  // };
 
-  addTask = () => {
-    this.props.addTaskFunc(this.state.taskDescription);
-  }
+  // addTask = () => {
+  //   this.props.addTaskFunc(this.state.taskDescription);
+  // }
 
-  taskDescriptionChanged = (event) => {
-    this.setState({
-      taskDescription: event.target.value
-    });
-    
-  }
+  // taskDescriptionChanged = (event) => {
+  //   this.setState({
+  //     item: event.target.value
+  //   });
+  // }
 
 
     render() {
+      const {item, handleChange, handleSubmit} = this.props
       return (
         <div className="row">
             <div className="inputField col-10">
-                <input value={this.state.taskDescription} className="form-control" placeholder="New task..." minLength="1" maxLength="100" onChange={this.taskDescriptionChanged} />
+                <input value={item} className="form-control" placeholder="New task..." minLength="1" maxLength="100" onChange={handleChange} />
             </div>
             <div className="addButton col-1">
-                <input type="submit" className="btn btn-light" value="Add" onClick={this.addTask}/>
+                <input type="submit" className="btn btn-light" value="Add" 
+                // onClick={this.addTask} 
+                onClick={handleSubmit}/>
             </div>
         </div>
       );
--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import uuidv4 from 'uuid/v4';
 class App extends React.Component {
 
   state = {
-    tasks: [
-      { id: uuidv4(), description: "Buy groceries", completed: false },
-      { id: uuidv4(), description: "Iron shirts", completed: false },
-      { id: uuidv4(), description: "Tidy room", completed: false }
-    ],
+    tasks: [],
+    id: uuidv4(),
+    description: "",
+    completed: false,
+    editItem: false,
 
     completedTasks: [
       { id: uuidv4(), description: "Walk the dog", completed: true },
@@ -19,39 +19,76 @@ class App extends React.Component {
     ]
   };
 
+  handleChange = (e) => {
+    this.setState({
+      description: e.target.value
+    });
+  };
 
-  addTask = (taskDescription) => {
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-    // Firstly define the task being added
     const taskToAdd = {
       id: uuidv4(),
-      description: taskDescription,
-      completed: false
-    };
-    // Get the current list of tasks from state
-    const currentTasks = this.state.tasks;
-    // Add the 'taskToAdd' to the array of tasks in state
-    currentTasks.push(taskToAdd);
-    // Update the state
+      description: this.state.description,
+      completed: false,
+      editItem: false
+    }
+
+    const currentTasks = [...this.state.tasks, taskToAdd];
+
     this.setState({
-      tasks: currentTasks
+      tasks: currentTasks,
+      description: ""
     });
-  }
+
+  };
+
+
+  // addTask = (taskDescription) => {
+
+  //   // Firstly define the task being added
+  //   const taskToAdd = {
+  //     id: uuidv4(),
+  //     description: taskDescription,
+  //     completed: false
+  //   };
+  //   // Get the current list of tasks from state
+  //   const currentTasks = this.state.tasks;
+  //   // Add the 'taskToAdd' to the array of tasks in state
+  //   currentTasks.push(taskToAdd);
+  //   // Update the state
+  //   this.setState({
+  //     tasks: currentTasks
+  //   });
+  // }
 
 
   editTask = (taskID) => {
-    const taskToEdit = this.state.tasks;
-    for (let i = 0; i < taskToEdit.length; i++) {
-      const task = taskToEdit[i];
+    const tasks = this.state.tasks;
+    const filteredTasks = tasks.filter(item => item.id !== taskID);
+    
+    const selectedItem = tasks.find(item => item.id === taskID)
 
-      if (task.id === taskID) {
-        task.description = null;
-        break;
-      }
-    }
-      this.setState({
-        tasks: taskToEdit
-      });
+    console.log(selectedItem);
+
+    this.setState({
+      tasks: filteredTasks,
+      description: selectedItem.description
+    });
+    // const taskToEdit = this.state.tasks;
+    // for (let i = 0; i < taskToEdit.length; i++) {
+    //   const task = taskToEdit[i];
+
+    //   if (task.id === taskID) {
+    //     task.description = null;
+    //     break;
+    //   }
+    // }
+    //   this.setState({
+    //     tasks: taskToEdit
+    //   });
+    console.log(taskID);
   }
 
 
@@ -122,7 +159,7 @@ class App extends React.Component {
           <Sidebar counterOutstanding={this.state.tasks.length} counterDone={this.state.completedTasks.length} />
           <div className="col-11 col-md-7">
             <div className="mainBody">
-              <AddNewTask addTaskFunc={this.addTask} />
+              <AddNewTask addTaskFunc={this.addTask} item={this.state.description} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
               <TaskArea
                 jobs={this.state.tasks}
                 doneJobs={this.state.completedTasks}
